6258e5efdbede13dbdd97b49181f57e8affe9fcb
JnrLouis <chibuezelouis@gmail.com>
2022-01-21 20:46:45 +0100

    First Commit

---
+++ dao/constants/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x8B62a5ABc3c586ef5C55362364caFAeB208387A7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
